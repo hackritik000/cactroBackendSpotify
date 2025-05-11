8a8f334aa18c582b98efc0dc45b77ddf213b9e4a
hackritik000 <hackritik000@gmail.com>
2025-05-11 13:17:15 +0530

    fixing redirect

--- src/app/api/spotify/callback/route.ts
+++ src/app/api/spotify/callback/route.ts
@@ -4,5 +4,10 @@ export async function GET(request: Request) {
   const url = new URL(request.url);
   const code = url.searchParams.get("code") || "";
   await handleCallback(code);
-  return Response.redirect("/", 302);
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: "/",
+    },
+  });
 }
