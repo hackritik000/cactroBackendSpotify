d07e57634bf2a764f2d55f2c0444bbd5ea524c1e
hackritik000 <hackritik000@gmail.com>
2025-05-11 12:41:50 +0530

    adding all api in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,38 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+      <h1>Home</h1>
+      <p>All API routes are exposed here</p>
+      <ul>
+        <li>
+          <a href="/api/spotify/login">/api/spotify/login</a>
+        </li>
+        <li>
+          <a href="/api/spotify/callback">/api/spotify/callback</a>
+        </li>
+        <li>
+          <a href="/api/spotify/play">/api/spotify/play</a>
+        </li>
+        <li>
+          <a href="/api/spotify/pause">/api/spotify/pause</a>
+        </li>
+        <li>
+          <a href="/api/spotify/now-playing">/api/spotify/now-playing</a>
+        </li>
+        <li>
+          <a href="/api/spotify/followed-artists">
+            /api/spotify/followed-artists
           </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </li>
+        <li>
+          <a href="/api/spotify/top-tracks">/api/spotify/top-tracks</a>
+        </li>
+        <li>
+          <a href="/api/spotify/test">/api/spotify/test</a>
+        </li>
+      </ul>
     </div>
   );
 }
