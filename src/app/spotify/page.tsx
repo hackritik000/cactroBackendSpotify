58da6ce44ae5c7bf401267ff24cf7a7fd8f4695f
hackritik000 <hackritik000@gmail.com>
2025-05-11 12:24:07 +0530

    fixing build

--- src/app/spotify/page.tsx
+++ src/app/spotify/page.tsx
@@ -1,6 +1,14 @@
-export default async function SpotifyPage() {
-  const res = await fetch("/api/spotify/top-tracks");
-  const data = await res.json();
+"use client";
+import { useEffect, useState } from "react";
+
+export default function SpotifyPage() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/spotify/top-tracks")
+      .then((res) => res.json())
+      .then(setData);
+  }, []);
 
   return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
